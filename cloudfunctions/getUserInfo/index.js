b0d405918cb359a521a4cfc93c95face3d51668a
qiaoyusen.rison <qiaoyusen.rison@bytedance.com>
2025-09-23 16:09:08 +0800

    init

---
+++ cloudfunctions/getUserInfo/index.js
@@ -0,0 +1,28 @@
+import cloud from 'wx-server-sdk'
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+const db = cloud.database()
+
+exports.main = async () => {
+  const { OPENID } = cloud.getWXContext()
+  try {
+    const user = await db.collection('users').where({ _openid: OPENID }).get()
+    if (user.data.length === 0) {
+      return {
+        code: 404,
+        message: '用户不存在'
+      }
+    }
+    return {
+      code: 200,
+      data: user.data[0]
+    }
+  } catch (err) {
+    console.error('获取用户信息失败:', err.message)
+    return {
+      code: 500,
+      message: '服务器错误'
+    }
+  }
+}
